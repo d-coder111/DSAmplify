2f47c1d11e4e793d58165698cdb4f1f3f7414cce
Shubham Kumar <72144322+shubhamraj01@users.noreply.github.com>
2023-10-07 11:24:00 +0530

    Update Min & Max in array.java

--- JAVA/Min & Max in array.java
+++ JAVA/Min & Max in array.java
@@ -13,7 +13,7 @@ class Main
 	static Pair getMinMax(int arr[]) 
 	{
 		Pair minmax = new Pair();
-		Arrays.sort(arr);             // Uses improved version of Quicksort - Tim sort ( first divide array into runs and sort by insertion sort, then merge sort on runs)
+		Arrays.sort(arr[]);             // Uses improved version of Quicksort - Tim sort ( first divide array into runs and sort by insertion sort, then merge sort on runs)
 		minmax.min = arr[0];
 		minmax.max = arr[arr.length - 1];
 		return minmax;
@@ -22,7 +22,7 @@ class Main
 	public static void main(String[] args) 
 	{
 		int arr[] = { 1000, 11, 445, 1, 330, 3000 };
-		Pair minmax = getMinMax(arr);
+		Pair minmax = getMinMax(arr[]);
 		System.out.println("Minimum element is " + minmax.min);
 		System.out.println("Maximum element is " + minmax.max);
 	}
