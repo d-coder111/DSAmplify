d9250deda1dcb713ffb021c1e2b4555339a7cb09
Harshal17S <harshal.ingale17@gmail.com>
2023-10-30 23:36:39 +0530

    Checking Number is Palindrome or Not using recursive method in Java

---
+++ JAVA/Palindrome.java
@@ -0,0 +1,22 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        int n=121;
+        if(rev(n)==n){
+            System.out.println("Palindrome");
+        }
+        else {
+            System.out.println("Not");
+        }
+
+    }
+    static int rev(int n){
+        int digit=(int)Math.log10(n)+1;
+        return helper(n,digit);
+    }
+    static int helper(int n,int digit){
+        if(n%10==n){
+            return n;
+        }
+        return n%10*(int)Math.pow(10,digit-1)+helper(n/10,digit-1);
+    }
+}
