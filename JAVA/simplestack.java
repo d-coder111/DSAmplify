174ad248cb19a3a8bec6e34287cbd9a800805712
Shubhal Gupta <107386458+shubhalgupta@users.noreply.github.com>
2023-11-02 14:12:42 +0530

    Fixes #6- Created simplestack.java
    Created a simple stack using Java with push, pop, and peek operations

---
+++ JAVA/simplestack.java
@@ -0,0 +1,67 @@
+import java.util.EmptyStackException;
+
+public class Stack<T> {
+    private Node<T> top;
+    private int size;
+
+    private static class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    public Stack() {
+        top = null;
+        size = 0;
+    }
+
+    public void push(T data) {
+        Node<T> newNode = new Node<>(data);
+        newNode.next = top;
+        top = newNode;
+        size++;
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        T data = top.data;
+        top = top.next;
+        size--;
+        return data;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return top.data;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        System.out.println("Stack size: " + stack.size());
+        System.out.println("Top element: " + stack.peek());
+
+        while (!stack.isEmpty()) {
+            System.out.println("Popped: " + stack.pop());
+        }
+    }
+}
