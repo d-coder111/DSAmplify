de1c54614520177004d1f6542f9a1d3b7eeee191
jenyyy4 <anuskaroy0410@gmail.com>
2024-10-04 16:49:46 +0530

    added a main function

--- JAVA/reverseWords.java
+++ JAVA/reverseWords.java
@@ -1,5 +1,14 @@
-class Solution {
-    public String reverseWords(String s) {
+import java.util.*;
+
+class ReverseWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println(reverse(s));
+        sc.close();
+    }
+    
+    static String reverse (String s) {
         String[] s2 = s.trim().split("\\s+");
         String s3 = "";
         for (int i = s2.length - 1; i > 0; i--) {
