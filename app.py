18a8cbf68e0d5a65d84fbedf7e5c1a5bc24f9863
baranwalayush <ayush6262kr@gmail.com>
2024-10-27 00:58:39 +0530

    Add problem_solution.html

--- app.py
+++ app.py
@@ -3,16 +3,19 @@ import os
 
 app = Flask(__name__)
 
+
 # default page
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 # render home page
 @app.route('/home')
 def home():
     """ Reder home page """
     return render_template('index.html')
+
 
 # render about page
 @app.route('/about')
@@ -37,5 +40,27 @@ def language():
 
     return render_template('index.html', questions=questions, language=language)
 
+
+@app.route('/problem/<filename>')
+def problem_solution(filename):
+    """
+    Read the solution code from the file and return the problem_solution.html page to show the solution code to user
+    """
+    # get the language which is passed as a parameter from the index.html
+    language = request.args.get('language')
+
+    if not language:
+        return "Language not specified. Please select a language." # Error Message if no Language is selected
+    
+    file_path = f"{language}/{filename}" # Filepath
+
+    with open(file_path, 'r') as f:
+        code = f.read()
+
+    question_name = filename.replace('_', ' ').replace('.cpp', '').replace('.java', '').replace('.py', '')
+
+    return render_template('problem_solution.html', code=code, question_name=question_name)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
